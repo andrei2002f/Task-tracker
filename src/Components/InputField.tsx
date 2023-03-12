b980589e65d877ebb49b6d51b5593e84c7695393
andrei2002f <farcasiu.andrei@gmail.com>
2023-03-12 18:52:28 +0200

    added files

---
+++ src/Components/InputField.tsx
@@ -0,0 +1,26 @@
+import "./styles.css";
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField = ({ todo, setTodo, handleAdd }: Props) => {
+  return (
+    <form className="input" onSubmit={(e) => handleAdd(e)}>
+      <input
+        type="input"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder="Enter a task"
+        className="input__box"
+      />
+      <button className="input_submit" type="submit">
+        Add
+      </button>
+    </form>
+  );
+};
+
+export default InputField;
